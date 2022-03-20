4192268f4830194a1d4e655fb617b53e5f25657c
Miracle <collinsalex50@gmail.com>
2022-03-20 23:40:03 +0100

    users and profiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:signup, keys: [:name])
-    devise_parameter_sanitizer.permit(:acount_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:acount_update, keys: [:name, :website, :bio])
   end
 end
